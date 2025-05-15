e1b8327f50cebe054158fc66ca6578f860a476af
dndjeh <dndjeh12@naver.com>
2025-05-15 21:18:32 +0900

    Update

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
 
 =======
 # main.py : 시작 모듈
 # function.py :Crawling을 위한 함수 모듈
-# OpenAI.py : openai를 사용하기 위한 함수 모듈듈
->>>>>>> 5b3320b398a9d57c27d5dcb94210be195c19bbcb
+# OpenAI.py : openai를 사용하기 위한 함수 모듈
+
--- SqlQurey/console.sql
+++ SqlQurey/console.sql
@@ -1,9 +1,24 @@
 create schema news_project;
+# drop schema news_project;
+# drop table news_project.search_information;
+
 
 create table news_project.search_information(
-    user_id bigint unsigned primary key auto_increment,
-    title varchar(50)NOT NULL ,
-    summary MEDIUMTEXT,
+    id bigint unsigned primary key auto_increment,
+    title varchar(200)NOT NULL ,
+    content MEDIUMTEXT,
     date DATE,
-    href TEXT
+    url TEXT
+);
+
+create table news_project.daum_financial_stock_table(
+    id bigint unsigned primary key auto_increment,
+    name varchar(50)NOT NULL ,
+    open_price INT,
+    high_price  INT,
+    low_price  INT,
+    close_price INT,
+    price_change INT,
+    trading_volume INT,
+    date DATE
 );
