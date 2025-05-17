020b8bf264e1c56441a7ccfb91868ad3e5642fa0
dndjeh <dndjeh12@naver.com>
2025-05-17 12:58:54 +0900

    모든 기능 모듈을 호출하는 엔트리 포인트

--- Daum_Crawling/main.py
+++ Daum_Crawling/main.py
@@ -10,7 +10,9 @@ import OpenAI as op
 import pandas as pd
 import financial_function as ff
 
+search = input("검색 하세요 : ")
+page_count = input("원하는 페이지 수 : ")
 
-ff.financial_search() # 다음 증권 크롤링
-#f.daum_search()
+#ff.financial_search(search, page_count) # 다음 증권 크롤링
+f.daum_search(search, page_count)
 #op.run(search) #OpenAI 모듈 실행
