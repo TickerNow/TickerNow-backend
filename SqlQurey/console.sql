043a5e7a348b8f9a36a44060369c7227bf197242
dndjeh <dndjeh12@naver.com>
2025-05-17 13:01:00 +0900

    Update

--- SqlQurey/console.sql
+++ SqlQurey/console.sql
@@ -1,7 +1,7 @@
-create schema news_project;
+# create schema news_project;
 # drop schema news_project;
-drop table news_project.search_information;
-
+# drop table news_project.search_information;
+# drop table news_project.daum_financial_stock_table;
 
 create table news_project.search_information(
     id bigint unsigned primary key auto_increment,
@@ -14,11 +14,17 @@ create table news_project.search_information(
 create table news_project.daum_financial_stock_table(
     id bigint unsigned primary key auto_increment,
     name varchar(50)NOT NULL ,
-    open_price INT,
-    high_price  INT,
-    low_price  INT,
-    close_price INT,
-    price_change INT,
-    trading_volume INT,
-    date DATE
+    open_price INT NOT NULL,
+    high_price  INT NOT NULL,
+    low_price  INT NOT NULL,
+    close_price INT NOT NULL,
+    price_change text NOT NULL,
+    change_rate text NOT NULL,
+    trading_volume INT NOT NULL,
+    date DATE NOT NULL
 );
+
+select *
+from news_project.daum_financial_stock_table
+where name LIKE '%SK%'
+ORDER BY date DESC ;
