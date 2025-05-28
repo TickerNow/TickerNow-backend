5afe10ccadac0bd9abaf751cd238db228cc709e2
dndjeh <dndjeh12@naver.com>
2025-05-28 23:12:55 +0900

    Update

--- SqlQurey/console.sql
+++ SqlQurey/console.sql
@@ -26,3 +26,26 @@ create table news_project.daum_financial_stock_table(
 
 select distinct (name)
 from news_project.daum_financial_stock_table;
+
+create table news_project.user_info(
+    user_id bigint unsigned primary key auto_increment,
+    name text NOT NULL,
+    sex char(2) NOT NULL,
+    age INT NOT NULL,
+    birth_date date NOT NULL,
+    nickname text NOT NULL,
+    id text NOT NULL,
+    password text NOT NULL,
+    joined_at  DATE NOT NULL
+);
+
+INSERT INTO news_project.user_info (name, sex, age, birth_date, nickname, id, password, joined_at)
+VALUES
+  ('park', '남자', 20, '2000-05-02', '뭐야', 'dndjeh', '1111', '2024-05-02'),
+  ('So', '여자', 21, '2003-07-14','아무거나', 'soso123', '2222', '2024-05-18'),
+  ('Kim','남자', 21,'2000-02-14', '이건아님','dign','3333','2025-04-12');
+
+drop table news_project.user_info;
+DELETE FROM news_project.user_info
+WHERE id = 'dndjeh';
+
