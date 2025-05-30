65553c6952435a30c5585a260d0b085965630c86
dndjeh <dndjeh12@naver.com>
2025-05-30 16:08:44 +0900

    회원 가입

--- Daum_Crawling/sign_up_function.py
+++ Daum_Crawling/sign_up_function.py
@@ -1,16 +1,20 @@
 from pyspark.sql import Row
 from pyspark.sql.functions import lit
+from passlib.hash import bcrypt
+
 
 def sign_up(spark, name, sex, age, birth_date, id, nickname, password, joined_at) :
-    """회원 정보 DB 저장장"""
+    """회원 정보 DB 저장"""
     ip = "127.0.0.1"
     port = "3306" 
     user = 'root'
-    passwd = '5941'
+    passwd = '5941' # 비밀번호 변경
     db = 'news_project'
     table_name = 'user_info'
 
     url = f"jdbc:mysql://{ip}:{port}/{db}"
+    
+    hashed_password = bcrypt.hash(password) # 비밀번호를 해시 함수로 암호화 처리
 
     # 1) 신규 사용자 정보를 담은 Row 객체 생성
     new_user = Row(
@@ -20,7 +24,7 @@ def sign_up(spark, name, sex, age, birth_date, id, nickname, password, joined_at
         birth_date=birth_date,
         id=id,
         nickname=nickname,
-        password=password, #암호화 처리 진행 해야함
+        password=hashed_password, 
         joined_at=joined_at
     )
 
@@ -36,7 +40,7 @@ def sign_up(spark, name, sex, age, birth_date, id, nickname, password, joined_at
         .option("password", passwd) \
         .mode("append") \
         .save()
-
+    
     return "회원가입이 완료 되었습니다."
     
 def id_check(spark, id):
@@ -97,7 +101,7 @@ def nickname_check(spark, nickname) :
     # SQL문을 사용하기 위해 View를 생성
     df.createOrReplaceTempView("user_info")
 
-    # 중복된 닉네임이이 존재하는지 확인
+    # 중복된 닉네임이 존재하는지 확인
     query = f"""
         select count(distinct(nickname)) as count
         from user_info
