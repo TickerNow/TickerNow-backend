3c321c18f3bbec7077e2f61f7ae0dcf5633944b0
dndjeh <dndjeh12@naver.com>
2025-06-10 19:47:17 +0900

    DB에 저장된 주식 정보 및 뉴스 조회

--- database_search_stock_information_function.py
+++ database_search_stock_information_function.py
@@ -1,21 +1,32 @@
 import json
 
 def get_stock_data_from_db(spark, stock_name):
-    '''DB를 조회하여 기업 주식 정보를 리턴'''
-    # 주식 정보 조회회
-    query = f"""
-        select *
-        from daum_financial_stock_table
-        where name = '{stock_name}' 
+    '''DB를 조회하여 기업 주식 정보 및 뉴스 정보를 JSON으로 리턴'''
+    
+    # 주식 데이터 쿼리
+    query_stock = f"""
+        SELECT *
+        FROM daum_financial_stock_table
+        WHERE name = '{stock_name}' 
         ORDER BY date DESC
-        """
+    """
+    df_stock = spark.sql(query_stock)
 
-    df = spark.sql(query)
+    # 뉴스 데이터 쿼리
+    query_news = f"""
+        SELECT *
+        FROM search_information
+        WHERE search = '{stock_name}' 
+        ORDER BY date DESC
+    """
+    df_news = spark.sql(query_news)
 
-    # Spark의 각 row를 JSON 문자열로 변환
-    json_rdd = df.toJSON()
+    # JSON 변환
+    stock_json = [json.loads(row) for row in df_stock.toJSON().collect()]
+    news_json = [json.loads(row) for row in df_news.toJSON().collect()]
 
-    # JSON 문자열을 Python dict로 파싱
-    json_list = [json.loads(row) for row in json_rdd.collect()]
-
-    return json_list
+    # 두 JSON 리스트를 하나의 딕셔너리로 묶어서 반환
+    return {
+        "stock_data": stock_json,
+        "news_data": news_json
+    }
